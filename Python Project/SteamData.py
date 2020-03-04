74da4dc94496098e4f188091ade454d83a098f76
TPogacar <pogacar.tami@gmail.com>
2020-03-04 20:02:15 +0100

    dodane metode, popravljena imena spremenljivk

--- Python Project/SteamData.py
+++ Python Project/SteamData.py
@@ -2,52 +2,58 @@ import re
 import requests
 
 
-class SteamUser:  # kaj če bi dala ime Gamer al kj tazga? Ker ga definirava kot enga igralca ne kot skupino
+class SteamUser:
     # en igralec
 
-    def __init__(self, userid):
-        """
-            initializira...
-            dobi ID uporabnika, shrani njegove podatke (njegovo stran, ime, št. iger, imena in id njegovih prijateljev)
-        """
-        self.steamUserID = userid  # št. uporabnika (ID)
-        self.steamProfileText = requests.get("https://steamcommunity.com/profiles/" + userid).text  # link
+    def __init__(self, userID):
+        self.steamUserID = userID
+        self.steamProfileText = requests.get("https://steamcommunity.com/profiles/" + userID).text
 
         # ime uporabnika
-        self.steamUserName = re.findall(r'<title>Steam Community :: .+</title>', self.steamProfileText)
-        self.steamUserName = re.sub(r'<[^>]+>', "", self.steamUserName[0])
-        self.steamUserName = re.sub(r'.+ :: ', "", self.steamUserName)
+        steamUserName = re.findall(r'<title>Steam Community :: .+</title>', self.steamProfileText)
+        steamUserName = re.sub(r'<[^>]+>', "", steamUserName[0])
+        self.steamUserName = re.sub(r'.+ :: ', "", steamUserName)
 
         # koliko iger ima uporabnik
-        self.numberOfGamesOwned = re.findall(r"\d+ games owned", self.steamProfileText)
-        self.numberOfGamesOwned = self.numberOfGamesOwned[0].split(" ")[0]
+        numberOfGamesOwned = re.findall(r"\d+ games owned", self.steamProfileText)
+        self.numberOfGamesOwned = numberOfGamesOwned[0].split(" ")[0]
 
 
     def __repr__(self):
-        return f'SteamUser({self.steamUserID})'
+        return self.__class__.__name__ + "(" + self.steamUserID + ")"
+
+
+    def getLevel(self):
+        '''
+            vrne level uporabnika
+        '''
+        level = re.findall(r'<span class="friendPlayerLevelNum">\d+</span></div>', self.steamProfileText)
+        level = re.sub(r'<[^>]+>', "", level[0])
+        return level
+
 
     def __str__(self):
-        return f'Uporabnik {self.steamUserName} igra {self.numberOfGamesOwned} različnih iger.' # mogoče se da "ima" namest "igra"
+        # pomembna sta nam predvsem level in število iger
+        return f'Uporabnik {self.steamUserName} je level {self.getLevel()} in ima {self.numberOfGamesOwned} različnih iger.'
 
-    # poleg imen so bi lahko še dobila podatke za njihov ID, Profilno sliko,
+
     def getFriends(self):
         '''
             vrne 2 tebeli: tabelo imen in tabelo id prijateljev
         '''
         # gre na stran uporabnika, kjer so prikazani vsi njegovi prijatelji
-        self.steamFriendsText = requests.get("https://steamcommunity.com/profiles/" + self.steamUserID + "/friends/").text
+        steamFriendsText = requests.get("https://steamcommunity.com/profiles/" + self.steamUserID + "/friends/").text
 
         # dobi imena vseh prijateljev in jih shrani v tabelo
-        self.steamFriendNames = re.findall(r'<div class="friend_block_content">.+<br>', self.steamFriendsText)
-        self.steamFriendNames = [re.sub(r'<[^>]+>', "", name) for name in self.steamFriendNames]
+        steamFriendNames = re.findall(r'<div class="friend_block_content">.+<br>', steamFriendsText)
+        steamFriendNames = [re.sub(r'<[^>]+>', "", name) for name in steamFriendNames]
         ##print(self.steamFriendNames, len(self.steamFriendNames))
 
         # dobi ID
-        self.steamFriendIDs = re.findall(r'data-steamid="\d+"', self.steamFriendsText)
-        self.steamFriendIDs = [re.sub(r'[^0-9]+', "", ID) for ID in self.steamFriendIDs]
-        ##print(self.steamFriendIDs, len(self.steamFriendIDs))
-
-        return self.steamFriendNames, self.steamFriendIDs  # (ime, ID)
+        steamFriendIDs = re.findall(r'data-steamid="\d+"', steamFriendsText)
+        steamFriendIDs = [re.sub(r'[^0-9]+', "", ID) for ID in steamFriendIDs]
+        
+        return steamFriendNames, steamFriendIDs  # (ime, ID)
 
     
     def howManyFriends(self):
@@ -57,72 +63,124 @@ class SteamUser:  # kaj če bi dala ime Gamer al kj tazga? Ker ga definirava kot
         return len(self.getFriends()[0])
 
 
-    def getSteamUserLevel(self):
-        '''
-            vrne level uporabnika
-        '''
-        self.steamUserLevel = re.findall(r'<span class="friendPlayerLevelNum">\d+</span></div>', self.steamProfileText)
-        self.steamUserLevel = re.sub(r'<[^>]+>', "", self.steamUserLevel[0])
-        return self.steamUserLevel
-
-
     def getFeaturedGames(self):
         '''
             vrne "Featured Games"
             igre ka jih ima uporabnik "raskzane", ponavad njegove najljubše/ najbol igrane
             raskazane ima lahko od 0-4
         '''
-        self.featuredGames = re.findall(r'<div class="showcase_slot showcase_gamecollector_game[^h]+href="https://steamcommunity.com/app/[^"]+', self.steamProfileText)
-        for i, game in enumerate(self.featuredGames):
-             self.featuredGames[i] = game.split('href="')[1]
-             self.featuredGames[i] = re.findall(r'<title>Steam Community :: .+</title>', requests.get(self.featuredGames[i]).text)
-             self.featuredGames[i] = re.sub(r'<[^>]+>', "", self.featuredGames[i][0])
-             self.featuredGames[i] = re.sub(r'.+ :: ', "", self.featuredGames[i])
-             self.featuredGames[i] = re.sub('™', '', self.featuredGames[i])
-             self.featuredGames[i] = re.sub('®', '', self.featuredGames[i])
-        return self.featuredGames
+        featuredGames = re.findall(r'<div class="showcase_slot showcase_gamecollector_game[^h]+href="https://steamcommunity.com/app/[^"]+', self.steamProfileText)
+        for i, game in enumerate(featuredGames):
+             thisGame = game.split('href="')[1]
+             thisGame = re.findall(r'<title>Steam Community :: .+</title>', requests.get(thisGame).text)
+             thisGame = re.sub(r'<[^>]+>', "", thisGame[0])
+             thisGame = re.sub(r'.+ :: ', "", thisGame)
+             thisGame = re.sub('™', '', thisGame)
+             featuredGames[i] = re.sub('®', '', thisGame)
+
+        return sorted(featuredGames, key=lambda x: x[1])
 
 
-    def getUserGames(self):
+    def getOwnedGames(self):
         '''
-            vrne seznam iger uporabnika, ter število ur na posamezni igri
+            vrne urejen slovar iger uporabnika ter pripadajoča števila ur (urejen glede na št. igranih ur)
         '''
-        self.steamGamesText = requests.get("https://steamcommunity.com/profiles/" + self.steamUserID + "/games/?tab=all", self.steamProfileText).text
-        self.steamGamesText = re.findall(r'<script language="javascript">[^<]+</script>', self.steamGamesText)
+        gamesText = requests.get("https://steamcommunity.com/profiles/" + self.steamUserID + "/games/?tab=all", self.steamProfileText).text
+        gamesText = re.findall(r'<script language="javascript">[^<]+</script>', gamesText)
 
-        # možn da se da bol simpl sam to deluje :D
         # igre
-        self.steamGames = re.findall(r'"name":"[^"]+"', self.steamGamesText[0])
-        self.steamGames = [re.sub(r'"name":"', "", game) for game in self.steamGames]
-        self.steamGames = [re.sub(r'"', "", game) for game in self.steamGames]
-        self.steamGames = [re.sub(r"\\[\w\d]+", "", game) for game in self.steamGames]
+        games = re.findall(r'"name":"[^"]+"', gamesText[0])
+        games = [re.sub(r'"name":"', "", game) for game in games]
+        games = [re.sub(r'"', "", game) for game in games]
+        games = [re.sub(r"\\[\w\d]+", "", game) for game in games]
 
         # čas igrane igre
-        self.steamGameTime = re.findall(r'"hours_forever":"[^"]+"', self.steamGamesText[0])
-        self.steamGameTime = [re.sub(r'"hours_forever":"', "", time) for time in self.steamGameTime]
-        self.steamGameTime = [re.sub(r'"', "", time) for time in self.steamGameTime]
+        gameTime = re.findall(r'"hours_forever":"[^"]+"', gamesText[0])
+        gameTime = [re.sub(r'"hours_forever":"', "", time) for time in gameTime]
+        gameTime = [re.sub(r'"', "", time) for time in gameTime]
 
-        self.steamGameDict = dict()
-        for i, game in enumerate(self.steamGames):
+        gameDict = dict()
+        for i, game in enumerate(games):
             try:
-                self.steamGameDict[game] = self.steamGameTime[i]
+                gameDict[game] = gameTime[i]
             except:
-                self.steamGameDict[game] = '0'
+                gameDict[game] = '0'
 
-        return self.steamGameDict
-        # ...
+        return gameDict
+
+
+    def getMostPopularGames(self):
+        '''
+            returns 10 the most played games (acording to the playing times),
+            among which are always all his featured games (regardless to the playing times)
+        '''
+        top = set(self.getFeaturedGames())
+        games = list(self.getOwnedGames().keys())
+        lenght = len(top)
+        for i, game in enumerate(games):
+            if lenght + i > 10:
+                break
+            top.add(game)
+        return sorted(top)
+
+
+    def getLikedGames(self):
+        '''
+            returns all games that user has played for more than 50 hour
+        '''
+        liked = []
+        ownedGames = self.getOwnedGames()
+        for game in ownedGames:
+            if float(ownedGames[game]) <= 50:
+                break
+            liked.append(game)
+        return liked
+
+
+
+
+
+class Friend(SteamUser):
+    # friends of our user
+    
+    def __init__(self, userID):
+        super().__init__(userID)
+
+
+    
+
+
+
+
+
+
+'''
+    za vsakega prijatelja našga uporabnika ustvariva svoj objekt Friend..
+    definirava:
+        skupni prijatelji (samo direktni)
+        top 3 prijatelji glede na
+            level
+            št. imetih iger
+        katere so najbolj igrane igre med prijatelji
+'''
+
+
 
 
 # ID mojga Steam Accounta ("Ajax")
 userID = "76561198069577640"
 jaz = SteamUser(userID)
-#print(jaz)
+# print(jaz)
 
-#print(jaz.getFriends())  # test
-#print(jaz.howManyFriends())  # test
-#print(jaz.getSteamUserLevel())  # test
-#print(jaz.getUserGames()) # test
-#print(jaz.getFeaturedGames())
+# TESTI METOD
+# print(jaz.getLevel())
+# print(jaz.getFriends())
+# print(jaz.howManyFriends())
+# print(jaz.getOwnedGames())
+# print(jaz.getFeaturedGames())
+# print(jaz.getMostPopularGames())
+# print(jaz.getLikedGames())
+
 
 
 # for ID in self.steamFriendIDs:
